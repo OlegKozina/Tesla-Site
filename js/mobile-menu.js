0278720cd3bb6c01db682cc9858c5589016aaed6
Oleg Kozina <olegkozina@Olegs-MacBook-Pro.local>
2024-03-01 15:35:01 -0700

    changed image names

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,27 +1,25 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const mobileOpen = document.querySelector('.menu_button');
-const mobileClose = document.querySelectorAll('.menu_close');
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileOpen = document.querySelector(".menu_button");
+const mobileClose = document.querySelectorAll(".menu_close");
 
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const toggleMenu = () => mobileMenu.classList.toggle("is-open");
 
-mobileOpen.addEventListener('click', toggleMenu);
-mobileClose.forEach((el)=>el.addEventListener('click', toggleMenu));
+mobileOpen.addEventListener("click", toggleMenu);
+mobileClose.forEach((el) => el.addEventListener("click", toggleMenu));
 
+$(document).ready(function () {
+  $(".mobile-menu__nav >li a").click(function (e) {
+    $(".tesla-title img").hide().attr("src", $(this).attr("href")).fadeIn(1000);
+    $(".tesla-title h1").text($(this).text());
+    $(".main-title").text($(this).text());
+    $(".footer-right h3").text($(this).text());
+    $(".model-change").text($(this).text());
 
-$(document).ready(function(){
-    $('.mobile-menu__nav >li a').click(function(e){
-
-        $('.tesla-title img').hide().attr('src', $(this).attr('href')).fadeIn(1000);
-        $('.tesla-title h1').text($(this).text());
-        $('.main-title').text($(this).text());
-        $('.footer-right h3').text($(this).text());
-        $('.model-change').text($(this).text());
-
-        e.preventDefault();
-        $('.mobile-menu__nav >li a').css({'color': 'white'}, {'border-bottom': 'none'})
-        $(this).css({'color': 'black'}, {'border-bottom': '2px solid white'})
-    })
-
-
-
-})
+    e.preventDefault();
+    $(".mobile-menu__nav >li a").css(
+      { color: "white" },
+      { "border-bottom": "none" }
+    );
+    $(this).css({ color: "black" }, { "border-bottom": "2px solid white" });
+  });
+});
--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,11 @@
-$(document).ready(function(){
-    $('.menu1 >li a').click(function(e){
+$(document).ready(function () {
+  $(".menu1 >li a").click(function (e) {
+    $(".tesla-title img").hide().attr("src", $(this).attr("href")).fadeIn(1000);
+    $(".tesla-title h1").text($(this).text());
+    $(".main-title").text($(this).text());
+    $(".footer-right h3").text($(this).text());
+    $(".model-change").text($(this).text());
 
-        $('.tesla-title img').hide().attr('src', $(this).attr('href')).fadeIn(1000);
-        $('.tesla-title h1').text($(this).text());
-        $('.main-title').text($(this).text());
-        $('.footer-right h3').text($(this).text());
-        $('.model-change').text($(this).text());
-
-        e.preventDefault();
-    })
-
-
-
-})
+    e.preventDefault();
+  });
+});
